f4a0fafccec4aae63c5ec29b8d988aecaecd1f97
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-30 01:27:33 -0400

    Create app.java

---
+++ atv04/app.java
@@ -0,0 +1,20 @@
+package atv04;
+
+import javax.swing.JOptionPane;
+
+public class app {
+    
+    public static void main(String[] args){
+
+        double nota[] = new double[4];
+        double atitudinal;
+        
+        atitudinal = Double.parseDouble(JOptionPane.showInputDialog(null, "Qual foi a nota do atitudinal?"));
+        MediaNota mediaNota = new MediaNota(nota, atitudinal);
+        
+        double media = mediaNota.media();
+        JOptionPane.showMessageDialog(null, String.format("A média é = %.2f", media));
+
+    }
+
+}
