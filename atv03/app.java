e4357f5958178d2c849563ae7d3200e2775271ca
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-30 00:35:48 -0400

    Create app.java

---
+++ atv03/app.java
@@ -0,0 +1,14 @@
+package atv03;
+
+import javax.swing.JOptionPane;
+
+public class app {
+    
+public static void main(String[] args){
+        double num;
+        ContarDigitosDeUmNum digitos1 = new ContarDigitosDeUmNum(num = Double.parseDouble(JOptionPane.showInputDialog(null, "Qual o número que irá ter seus digitps contados?")));
+
+    }
+
+
+}
