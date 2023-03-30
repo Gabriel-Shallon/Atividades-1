3353d3e7276721f36a4bddd53572ba23ec071094
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-30 01:27:36 -0400

    Create MediaNota.java

---
+++ atv04/MediaNota.java
@@ -0,0 +1,34 @@
+package atv04;
+
+import javax.swing.JOptionPane;
+
+public class MediaNota {
+    
+    protected double nota[];
+    protected double atitudinal;
+    protected double media;
+    
+    MediaNota(double nota[], double atitudinal){
+
+       this.nota = nota;
+       this.atitudinal = atitudinal; 
+
+    }
+
+    public double media(){
+        
+       int avaquant = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantas avaliações foram feitas?(No máximo 4)"));
+
+       for (int i = 0; i < avaquant; i++){
+
+       nota[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Qual foi a nota da "+(i+1)+"° avaliação?"));
+
+       }
+
+       
+
+       media = ((nota[0]+nota[1]+nota[2]+nota[3])/avaquant)*0.8+atitudinal;
+        return media;
+    }
+
+}
